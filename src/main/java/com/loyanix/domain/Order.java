1486312fefc4f4d4fb8b20cf5afa3508ee23102a
LoyaniX <loyanmaksim@gmail.com>
2019-02-11 20:34:33 +0200

    Added Order and Product model

---
+++ src/main/java/com/loyanix/domain/Order.java
@@ -0,0 +1,11 @@
+package com.loyanix.domain;
+
+import java.util.List;
+
+public class Order {
+
+    private long id;
+    private Client client;
+    private List<Product> products;
+
+}
---
+++ src/main/java/com/loyanix/domain/Product.java
@@ -0,0 +1,11 @@
+package com.loyanix.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private BigDecimal price;
+    private int quantity;
+}
